514e9a8c2e98839a21f33f1c95877b819f87e2d8
Sabrina <sabrinabuenoprata@gmail.com>
2023-08-21 18:47:41 -0300

    adicao de adicao e busca de videos

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,7 +2,7 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard(titulo, descricao, url, imagem) {
+export default function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
     video.innerHTML = `
